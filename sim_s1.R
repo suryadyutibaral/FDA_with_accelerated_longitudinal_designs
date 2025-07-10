ce6ef7a4c7c3c9584e1137b1cf88fb12bc3b2b63
suryadyutibaral <sdbaral@ucdavis.edu>
2025-07-09 17:09:57 -0700

    Add files via upload
    Running simulations for section 1 of the paper. R functions contain function to generate true trajectories, generate accelerated data, and evaluate simulated trajectories. Evaluation returns optimized and all MSEs, biases, ISEs and IEs.
    
    FDA_ALD_sim is an example code of one run with visual plots.

---
+++ sim_s1.R
@@ -0,0 +1,129 @@
+library(pracma)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(parallel)
+library(fda)
+begin = Sys.time()
+source("C:/Users/sonnet/Box/FDA/Dynamic-Modeling-of-Functional-Snippets-main/code/ldm.R")
+source("C:/Users/sonnet/Box/FDA/Dynamic-Modeling-of-Functional-Snippets-main/code/kerFctn.R")
+source("C:/Users/sonnet/Box/FDA/FDA_paper/Rfunctions.R")
+
+sim_S1 <- function(method, rep_cond, iter) {
+  set.seed(iter)
+  results <- list()
+  n_subj <- 250
+  tfine <- seq(0, 19, length.out = 240)
+  
+  # Simulate true data and sparse sampling
+  sim_data <- simulate_exponential_decay(n_subjects = n_subj, x = tfine, noise = 0.1)
+  sim_al_data <- simulate_accelerated_longitudinal(Y_full = sim_data$trajectories, n_time_points = 3)
+  
+  age <- 0:19
+  
+  # Generate initial values depending on method
+  if (method == "normal") {
+    z0 <- generate_initial_values(
+      Lt_list = sim_al_data$Lt,
+      Ly_list = sim_al_data$Ly,
+      age_grid = age,
+      m = 1,
+      method = "normal"
+    )
+    z02 <- generate_initial_values(
+      Lt_list = sim_al_data$Lt,
+      Ly_list = sim_al_data$Ly,
+      age_grid = age,
+      m = rep_cond,
+      method = "normal"
+    )
+  } else if (method == "replicate") {
+    z0 <- generate_initial_values(
+      Lt_list = sim_data$Lt_true,
+      Ly_list = sim_data$Ly_true,
+      age_grid = age,
+      m = 1,
+      method = "replicate"
+    )
+    z02 <- generate_initial_values(
+      Lt_list = sim_data$Lt_true,
+      Ly_list = sim_data$Ly_true,
+      age_grid = age,
+      m = rep_cond,
+      method = "replicate"
+    )
+  } else {
+    stop("Invalid method. Use 'normal' or 'replicate'.")
+  }
+  
+  # Fit models
+  acd_fda <- ldm(sim_al_data$Ly, sim_al_data$Lt, z0, age,
+                 optns = list(M = nrow(z0), cores = 24)
+  )
+  
+  acd_fda_2 <- ldm(sim_al_data$Ly, sim_al_data$Lt, z02, age,
+                   optns = list(
+                     M = nrow(z02),
+                     bw1 = acd_fda$optns$bw1,
+                     bw2 = acd_fda$optns$bw2,
+                     regular = FALSE, cores = 24
+                   )
+  )
+  
+  # Evaluate final model
+  results <- evaluate_simulations(tfine = tfine, sim_al_data = sim_al_data, sim_data = sim_data, acd_fda_2 = acd_fda_2)
+  rm(sim_data, sim_al_data, acd_fda, acd_fda_2)
+  
+  # Return results
+  return(list(
+    ie_opt = results$ie_opt,
+    ie_all = results$ie_all,
+    ise_opt = results$ise_opt,
+    ise_all = results$ise_all,
+    mse_opt = results$mse_opt,
+    mse_all = results$mse_all,
+    bias_opt = results$bias_opt,
+    bias_all = results$bias_all
+  ))
+}
+
+methods <- c("normal", "replicate")
+rep_conds <- c(4,10,100)
+iters <- 1:20
+
+# Initialize result tibble
+result_tbl <- tibble(iter = iters)
+
+# Loop through method and repetition conditions
+for (method in methods) {
+  for (m in rep_conds) {
+    
+    # Create column name for results
+    col_name <- paste0(method, "_m", m)
+    
+    # Print start of this condition
+    message("Starting simulations for method = ", method, ", m = ", m)
+    
+    # Run sim_S1 for all iterations with progress message
+    result_tbl[[col_name]] <- map(iters, function(i) {
+      message("  Iteration ", i, " for ", method, "_m", m)
+      
+      # Wrap sim_S1 in tryCatch to handle errors
+      tryCatch(
+        sim_S1(method = method, rep_cond = m, iter = i),
+        error = function(e) {
+          message("    Error at iter ", i, ": ", conditionMessage(e))
+          return(NA)
+        }
+      )
+    })
+  }
+}
+
+saveRDS(result_tbl, file = "C:/Users/sonnet/Box/FDA/FDA_paper/result_tbl")
+result_tbl <- readRDS(file = "C:/Users/sonnet/Box/FDA/FDA_paper/result_tbl")
+
+end = Sys.time()
+end - begin
